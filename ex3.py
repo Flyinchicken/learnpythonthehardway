e80c6be4f6d226ec7c306133b41b71bafca72d77
Flyinchicken <twj3454@hotmail.com>
2021-08-02 19:44:37 -0400

    2021-Aug-02 19:44 updates

---
+++ ex3.py
@@ -0,0 +1,26 @@
+# This excercise allows me to practice printing and math operators
+print("I will now count my chickens:")
+
+# The order of the math operations matters
+print("Hens", 25.0 + 30.0 / 6.0) # 30
+print("Roosters", 100.0 - 25.0 * 3.0 % 4.0) # 97
+
+print("Now I will count the eggs:")
+
+print(3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0) # 6.75
+
+print("Is it true that 3 + 2 < 5 - 7?") # False
+
+print(3.0 + 2.0 < 5.0 - 7.0) # this prints out a boolean
+
+print("What is 3 + 2?", 3.0 + 2.0) # 5
+print("What is 5 - 7?", 5.0 - 7.0) # -2
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+
+# prints strings and booleans
+print("Is it greater?", 5.0 >= -2.0) # True
+print("Is it greater or equal?", 5.0 >= -2.0) # True
+print("Is it less or equal?", 5.0 <= -2.0) # False
